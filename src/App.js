abd93897fb1ada9041b0889e02601e9ff77dd55c
dbuiviet <dbuiviet@gmail.com>
2020-01-13 17:47:57 +0700

    143 DN

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.cpn';
 
+import { Switch, Route } from 'react-router-dom';
+
+const Hatspage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+);
+
 
 function App() {
     return (
-        <div className="App">
-            <Homepage />
+        <div>
+            <Switch>
+                <Route exact path='/' component={Homepage} />
+                <Route path='/hats' component={Hatspage} />
+            </Switch>
         </div>
+
+        // <div className="App">
+        //     <Homepage />
+        // </div>
     );
 }
 
--- src/pages/homepage/homepage.cpn.jsx
+++ src/pages/homepage/homepage.cpn.jsx
@@ -5,7 +5,6 @@ import Directory from '../../components/directory/directory.cpn';
 
 const Homepage = () => (
     <div className='homepage'>
-        <h1>Welcome to my Homepage</h1>
         <div className='directory-menu'>
             <Directory />
         </div>
