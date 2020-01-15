6667365b52d8b145e29a469719887a479b6f361d
dbuiviet <dbuiviet@gmail.com>
2020-01-15 16:35:12 +0700

    143 DN

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.cpn';
-import ShopPage from "./pages/shop/shop.cpn";
+import ShopPage from './pages/shop/shop.cpn';
+import Header from './components/header/header.cpn';
 
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path='/' component={Homepage} />
                 <Route path='/shop' component={ShopPage} />
